cc09b19131f29b56dc24d1ef6455ecacb5c1edde
Omar Ryhan <omarryhan@gmail.com>
2019-05-21 01:41:27 +0200

    Boilerplate #3

---
+++ src/constants/index.js
@@ -0,0 +1,40 @@
+export const COLOR_PALETTE = Object.freeze({
+    blue: {
+        lightAlt: 'rgb(97, 184, 196)',
+        lightAlt2: 'rgb(2, 137, 140)',
+        light: '#005E7C',
+        primary: '#001242',
+        dark: '#000022',
+    },
+    black: {
+        primary: 'rgb(28, 27, 32)',
+    },
+    white: {
+        primary: '#fff',
+    },
+    fuschia: {
+        primary: 'rgb(235, 71, 126)',
+    },
+});
+
+export const SCHOOLS_THEME = Object.freeze({
+    color: COLOR_PALETTE.black.primary,
+    backgroundColor: COLOR_PALETTE.white.primary,
+    scrollbarHandleColor: COLOR_PALETTE.blue.primary,
+});
+
+export const STUDENTS_THEME = Object.freeze({
+    color: COLOR_PALETTE.black.primary,
+    backgroundColor: COLOR_PALETTE.white.primary,
+    scrollbarHandleColor: COLOR_PALETTE.blue.primary,
+});
+
+export const EMPLOYERS_THEME = Object.freeze({
+    color: COLOR_PALETTE.black.primary,
+    backgroundColor: COLOR_PALETTE.white.primary,
+    scrollbarHandleColor: COLOR_PALETTE.blue.primary,
+});
+
+export const FONT_SIZES = Object.freeze({
+
+});
