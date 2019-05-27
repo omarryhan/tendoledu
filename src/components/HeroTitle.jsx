ce42c0eb7955b2ee016a03d7e77dcc44333da01e
Gustavo <gustavo.gigante.s@gmail.com>
2019-05-27 22:44:27 +0100

    Changes to Hero Title size

--- src/components/HeroTitle.jsx
+++ src/components/HeroTitle.jsx
@@ -3,19 +3,19 @@ import { media } from '../constants';
 
 
 export default styled.h1`
-    font-size: 4.5em;
+    font-size: 4em;
     font-weight:bold;
 
     ${media.lessThan('laptop')`
-        font-size: 3em;
+        font-size: 2.7em;
     `};
 
     ${media.lessThan('tablet')`
-        font-size: 2.5em;
+        font-size: 2.2em;
     `};
 
     ${media.lessThan('mobileMedium')`
-        font-size: 2em;
+        font-size: 1.8em;
     `};
 
     text-align: left;
