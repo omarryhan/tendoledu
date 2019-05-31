a7866778c9993e78bfd8e9d2fd1310ce78555483
Omar Ryhan <omarryhan@gmail.com>
2019-05-31 13:45:02 +0200

    Added missing component

---
+++ src/components/JustSignedUpAlerts.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import { useAlert } from 'react-alert';
+
+
+/* eslint-disable react-hooks/exhaustive-deps */
+const JustSignedUpAlerts = ({ alertDefinitions }) => {
+    const alert = useAlert();
+    useEffect(() => {
+        alertDefinitions.forEach(({ message, waitTime, alertTime }) => {
+            setTimeout(
+                () => alert.info(message, { timeout: alertTime }),
+                waitTime,
+            );
+        });
+    },
+    []);
+    return null;
+};
+
+JustSignedUpAlerts.propTypes = {
+    alertDefinitions: PropTypes.arrayOf(PropTypes.shape({
+        message: PropTypes.string.isRequired,
+        waitTime: PropTypes.number.isRequired,
+        alertTime: PropTypes.number.isRequired,
+    })).isRequired,
+};
+
+export default JustSignedUpAlerts;
